b43f358ec0af99cfb8770cbb90ef45601f95d45d
PlatinMavi <92332324+PlatinMavi@users.noreply.github.com>
2023-03-24 00:54:47 +0300

    MangaView
    MangaView yapıldı ve yeni bölümler düzenlendi

--- MangaBridge/Manga/models.py
+++ MangaBridge/Manga/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
 class Manga(models.Model):
     manga_name = models.CharField(max_length= 255, default="null")
     manga_image = models.ImageField(upload_to="thumbnail", default="thumbnail/noimage.png")
-    manga_views=models.IntegerField(default=0)
+    manga_views= models.IntegerField(default=0)
+    manga_description = models.TextField()
 
     def __str__(self):
         return f"{self.id}, {self.manga_name}"
@@ -25,5 +27,14 @@ class Chapter(models.Model):
 
     def __str__(self):
         return f"{self.chapter_number}, {self.manga}, {self.fansub}"
-
     
+class Duyuru(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1024)
+
+
+class Categorys(models.Model):
+    Category_name = models.CharField(max_length=255)
+class MangaCategory(models.Model):
+    category = models.ForeignKey(Categorys, on_delete=models.CASCADE)
+    manga = models.ForeignKey(Manga, on_delete=models.CASCADE)
